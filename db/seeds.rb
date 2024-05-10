2148a4fb19131ab5767f5501ce88e3b131458878
TalhaHMTTK <talhahmttk2@gmail.com>
2024-05-10 18:26:44 +0500

    I created this app using action cable

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+User.create!(email: "dean@example.com",
+            password: "password",
+            password_confirmation: "password")
+
+User.create!(email: "john@doe.com",
+            password: "password",
+            password_confirmation: "password")
+
+Room.create!(name: 'General')
+
+Room.create!(name: 'Testing')
+
+Room.create!(name: 'Random')
